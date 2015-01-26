1ef1c1b090515c6209654dd6e6fcf6bee71be9bc
benawitan <choonyan@gmail.com>
2015-01-27 01:16:55 +0800

    First commit after pod install mixpanel

--- Client/Pods/Target Support Files/Pods/Pods-environment.h
+++ Client/Pods/Target Support Files/Pods/Pods-environment.h
@@ -90,3 +90,15 @@
 #define COCOAPODS_VERSION_MINOR_Braintree_Venmo 5
 #define COCOAPODS_VERSION_PATCH_Braintree_Venmo 0
 
+// Mixpanel
+#define COCOAPODS_POD_AVAILABLE_Mixpanel
+#define COCOAPODS_VERSION_MAJOR_Mixpanel 2
+#define COCOAPODS_VERSION_MINOR_Mixpanel 6
+#define COCOAPODS_VERSION_PATCH_Mixpanel 2
+
+// Mixpanel/MPCategoryHelpers
+#define COCOAPODS_POD_AVAILABLE_Mixpanel_MPCategoryHelpers
+#define COCOAPODS_VERSION_MAJOR_Mixpanel_MPCategoryHelpers 2
+#define COCOAPODS_VERSION_MINOR_Mixpanel_MPCategoryHelpers 6
+#define COCOAPODS_VERSION_PATCH_Mixpanel_MPCategoryHelpers 2
+
